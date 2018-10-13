69a53fa257caf34746b665c70a65522578d1a6e4
Nat Reid <nathanielreid@gmail.com>
2018-10-13 01:33:30 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = new Object({dog  : 'dog music'})
 function updatePlaylist(playlist, artistName, songTitle){
-  Object.assign(playlist, {[artistName] : songTitle
+  Object.assign(playlist, {[artistName] : songTitle})
   return playlist
 }
 function removeFromPlaylist(playlist, artistName){
